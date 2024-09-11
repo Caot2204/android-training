d93c06144e717c3d2ace23b5790093ce70f08ff8
Caot22 <caot2204@gmail.com>
2024-09-11 13:36:32 -0600

    more codelabs

---
+++ basic-android-kotlin-compose-training-mars-photos-starter/app/src/test/java/com/example/marsphotos/fake/MarsViewModelTest.kt
@@ -0,0 +1,27 @@
+package com.example.marsphotos.fake
+
+import com.example.marsphotos.fake.rules.TestDispatcherRule
+import com.example.marsphotos.ui.screens.MarsUiState
+import com.example.marsphotos.ui.screens.MarsViewModel
+import junit.framework.Assert.assertEquals
+import kotlinx.coroutines.test.runTest
+import org.junit.Rule
+import org.junit.Test
+
+class MarsViewModelTest {
+
+    @get:Rule
+    val testDispatcher = TestDispatcherRule()
+
+    @Test
+    fun marsViewModel_getMarsPhotos_verifyMarsIuStateSuccess() =
+        runTest {
+            val marsViewModel = MarsViewModel(
+                marsPhotosRepository = FakeNetworkMarsPhotosRepository()
+            )
+            assertEquals(
+                MarsUiState.Success(FakeDataSource.photosList),
+                marsViewModel.marsUiState
+            )
+        }
+}
