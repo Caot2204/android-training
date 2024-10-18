e789f5019643f3406f5c292ad1dfd6a325aa6018
Caot22 <caot2204@gmail.com>
2024-10-18 14:56:13 -0600

    WorkManager pathway

---
+++ basic-android-kotlin-compose-training-workmanager-starter/app/src/androidTest/java/com/example/bluromatic/WorkInstrumentationTest.kt
@@ -0,0 +1,69 @@
+package com.example.bluromatic
+
+import android.content.Context
+import androidx.test.core.app.ApplicationProvider
+import androidx.work.Data
+import androidx.work.ListenableWorker
+import androidx.work.testing.TestListenableWorkerBuilder
+import androidx.work.workDataOf
+import com.example.bluromatic.workers.BlurWorker
+import com.example.bluromatic.workers.CleanupWorker
+import com.example.bluromatic.workers.SaveImageToFileWorker
+import junit.framework.Assert.assertTrue
+import kotlinx.coroutines.runBlocking
+import org.junit.Before
+import org.junit.Test
+
+class WorkInstrumentationTest {
+
+    private lateinit var context: Context
+    val mockUriInput = KEY_IMAGE_URI to "android.resource://com.example.bluromatic/drawable/android_cupcake"
+    @Before
+    fun setup() {
+        context = ApplicationProvider.getApplicationContext()
+    }
+
+    @Test
+    fun cleanupWorker_doWork_resultSuccess() {
+        val worker = TestListenableWorkerBuilder<CleanupWorker>(context).build()
+        runBlocking {
+            val result = worker.doWork()
+            assertTrue(result is ListenableWorker.Result.Success)
+        }
+    }
+
+    @Test
+    fun blurWorker_doWork_resultSuccessReturnsUri() {
+        val worker = TestListenableWorkerBuilder<BlurWorker>(context)
+            .setInputData(workDataOf(mockUriInput))
+            .build()
+        runBlocking {
+            val result = worker.doWork()
+            val resultUri = result.outputData.getString(KEY_IMAGE_URI)
+            assertTrue(result is ListenableWorker.Result.Success)
+            assertTrue(result.outputData.keyValueMap.containsKey(KEY_IMAGE_URI))
+            assertTrue(
+                resultUri?.startsWith("file:///data/user/0/com.example.bluromatic/files/blur_filter_outputs/blur-filter-output-")
+                ?: false
+            )
+        }
+    }
+
+    @Test
+    fun saveImageToFileWorker_doWork_resultSuccessReturnsImageUrl() {
+        val worker = TestListenableWorkerBuilder<SaveImageToFileWorker>(context)
+            .setInputData(workDataOf(mockUriInput))
+            .build()
+        runBlocking {
+            val result = worker.doWork()
+            val resultUri = result.outputData.getString(KEY_IMAGE_URI)
+            assertTrue(result is ListenableWorker.Result.Success)
+            assertTrue(result.outputData.keyValueMap.containsKey(KEY_IMAGE_URI))
+            assertTrue(
+                resultUri?.startsWith("content://media/external/images/media/")
+                    ?: false
+            )
+        }
+    }
+
+}
